658c73dd3cd398dd1649b5ff970f61258b767db5
Ashutosh Singh <68840431+Ashutosh-Tester@users.noreply.github.com>
2022-10-06 12:59:09 +0530

    updated

--- src/arrays/min_num_jumps.java
+++ src/arrays/min_num_jumps.java
@@ -26,6 +26,7 @@ class Solution{
     public static void main(String []args)
     {
     int arr[]={1,3,5,8,9,2,6,7,6,8,9};
+        int n=arr.length;
     System.out.println( minjumps(arr));
     }
 }
